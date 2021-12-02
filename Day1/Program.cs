30bfc79f1446c596728978b9ed711ee6d9a0b8d8
Snivo <jrgerba@gmail.com>
2021-12-01 20:28:36 -0500

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Day1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Sonar sonar = new();
+
+        StreamReader inputs = new(File.Open("input.txt", FileMode.Open, FileAccess.Read));
+
+        string? input = inputs.ReadLine();
+        while (input != null)
+        {
+            sonar.NextInput(int.Parse(input));
+            input = inputs.ReadLine();
+        }
+
+        Console.WriteLine($"Total depth increases: {sonar.DMI}");
+        Console.WriteLine($"Total window depth increases: {sonar.WDMI}");
+    }
+}
---
+++ Day1/Sonar.cs
@@ -0,0 +1,30 @@
+﻿namespace Day1
+{
+    internal class Sonar
+    {
+        public readonly List<int> Inputs = new();
+        private readonly List<int> windowInputs = new();
+        ///<summary>Depth Measurement Increases</summary>
+        public int DMI { get; private set; } = 0;
+
+        ///<summary>Windowed Depth Measurement Increases</summary>
+        public int WDMI { get; private set; } = 0;
+
+        public void NextInput(int input)
+        {
+            Inputs.Add(input);
+
+            // Increment DMI if the depth measurement increases
+            if (Inputs.Count > 1)
+                DMI += input > Inputs[^2] ? 1 : 0;
+
+            if (Inputs.Count > 2)
+            {
+                windowInputs.Add(Inputs[^1] + Inputs[^2] + Inputs[^3]);
+
+                if (windowInputs.Count > 1)
+                    WDMI += windowInputs[^1] > windowInputs[^2] ? 1 : 0;
+            }
+        }
+    }
+}
